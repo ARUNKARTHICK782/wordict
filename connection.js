e8efd2dfe7cc578fa105f6d929f566fda2e3f9f6
ARUNKARTHICK782 <arunkarthick.cs20@bitsathy.ac.in>
2023-03-02 09:16:55 +0530

    env variables added

--- connection.js
+++ connection.js
@@ -1,9 +1,12 @@
 const mongoose  = require('mongoose');
-
+const config = require('config');
 
 async function connectToMongoDB()
 {
-    await mongoose.connect('mongodb+srv://arunkarthickm:arunkarthick007@mak.psstokh.mongodb.net/?retryWrites=true&w=majority')
+    const username = config.get("DB_USERNAME");
+    const password = config.get("DB_PASSWORD");
+    const CONNECTION_STRING = `mongodb+srv://${username}:${password}@mak.psstokh.mongodb.net/?retryWrites=true&w=majority`;
+    await mongoose.connect(CONNECTION_STRING)
     .then(()=>{
             console.log("Connected to MongoDB");
     });
